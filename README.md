463a9be27cfa3a783b89b3a5de50d352dd3d7043
Ryan Weppler <eatsleeptraincode@gmail.com>
2012-06-16 15:26:31 -0500

    added edit athlete functionality

--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 A scoreboard to log CrossFit workouts for the crew at CrossFit204.
 
+ToDo:
+Change Password
+Encrypt password
+Move login status to partial (overcome stack overflow exception)
+Update Score
+Delete Score
+How does changing athlete info change scoreboard?
+Search
+Edit Workout
+Workout Details
+Authorization
+Validation
---
+++ src/CrossFit204ScoreBoard.Web/Actions/Athletes/EditAction.cs
@@ -0,0 +1,44 @@
+﻿using CrossFit204ScoreBoard.Web.Models;
+using FubuMVC.Core;
+using FubuMVC.Core.Continuations;
+using Raven.Client;
+
+namespace CrossFit204ScoreBoard.Web.Actions.Athletes
+{
+    public class EditAction
+    {
+        private readonly IDocumentSession session;
+
+        public EditAction(IDocumentSession session)
+        {
+            this.session = session;
+        }
+
+        public EditAthleteViewModel Get(EditAthleteRequest request)
+        {
+            var athlete = session.Load<Athlete>("athletes/" + request.AthleteId);
+            return new EditAthleteViewModel {Athlete = athlete};
+        }
+
+        public FubuContinuation Post(EditAthleteViewModel request)
+        {
+            var athlete = session.Load<Athlete>(request.Athlete.Id);
+            athlete.FirstName = request.Athlete.FirstName;
+            athlete.LastName = request.Athlete.LastName;
+            athlete.Gender = request.Athlete.Gender;
+            return FubuContinuation.RedirectTo(new AthleteDetailsRequest {AthleteId = request.Athlete.Id.Replace("athletes/","")});
+        }
+         
+    }
+
+    public class EditAthleteRequest
+    {
+        [RouteInput]
+        public string AthleteId { get; set; }
+    }
+
+    public class EditAthleteViewModel
+    {
+        public Athlete Athlete { get; set; }
+    }
+}
