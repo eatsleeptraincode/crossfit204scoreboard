0b0a7d763873642579da6b7c2927888f3b16a568
Ryan Weppler <eatsleeptraincode@gmail.com>
2012-06-23 08:05:52 -0500

    fixed unit test

--- src/CrossFit204ScoreBoard.Tests/Actions/Athletes/EditActionTests.cs
+++ src/CrossFit204ScoreBoard.Tests/Actions/Athletes/EditActionTests.cs
@@ -1,5 +1,6 @@
 ﻿using CrossFit204ScoreBoard.Web.Actions.Athletes;
 using CrossFit204ScoreBoard.Web.Models;
+using FubuMVC.Core.Continuations;
 using FubuTestingSupport;
 using NUnit.Framework;
 using Raven.Client;
@@ -22,12 +23,25 @@ namespace CrossFit204ScoreBoard.Tests.Actions.Athletes
     [TestFixture]
     public class EditActionPostTests : InteractionContext<EditAction>
     {
+        Athlete athlete = new Athlete { Id = "Id" };
+        private FubuContinuation result;
+
+        protected override void beforeEach()
+        {
+            Services.Get<IDocumentSession>().Stub(s => s.Load<Athlete>(athlete.Id)).Return(athlete);
+            result = ClassUnderTest.Post(new EditAthleteViewModel { Athlete = athlete });
+        }
+
         [Test]
         public void GetAthlete()
         {
-            var athlete = new Athlete {Id = "Id"};
-            ClassUnderTest.Post(new EditAthleteViewModel { Athlete = athlete });
             Services.Get<IDocumentSession>().AssertWasCalled(s => s.Load<Athlete>(athlete.Id));
+        }
+
+        [Test]
+        public void RedirectToAthleteDetails()
+        {
+            result.AssertWasRedirectedTo<AthleteDetailsRequest>(r => r.AthleteId == athlete.Id);
         }
     }
 }
