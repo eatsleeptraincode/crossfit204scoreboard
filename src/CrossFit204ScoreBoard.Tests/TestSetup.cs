3904ebe69b64cb2dc37a7c440dbcc0c403348fcd
Ryan Weppler <eatsleeptraincode@gmail.com>
2012-07-07 11:11:12 -0500

    attempt to fix running tests on server

--- src/CrossFit204ScoreBoard.Tests/TestSetup.cs
+++ src/CrossFit204ScoreBoard.Tests/TestSetup.cs
@@ -1,6 +1,13 @@
-﻿using CrossFit204ScoreBoard.Web.Config;
+﻿using CrossFit204ScoreBoard.Web.Actions;
+using CrossFit204ScoreBoard.Web.Behaviours;
+using CrossFit204ScoreBoard.Web.Config;
+using CrossFit204ScoreBoard.Web.Security;
 using FubuMVC.Core;
+using FubuMVC.Core.Behaviors;
+using FubuMVC.Core.Security.AntiForgery;
 using FubuMVC.StructureMap;
+using FubuMVC.Validation;
+using FubuValidation;
 using NUnit.Framework;
 
 namespace CrossFit204ScoreBoard.Tests
@@ -12,9 +19,44 @@ namespace CrossFit204ScoreBoard.Tests
         public void Init()
         {
             FubuApplication
-                .For<ConfigureFubuMvc>()
+                .For<TestRegistry>()
                 .StructureMapObjectFactory()
                 .Bootstrap();
+        }
+    }
+
+    public class TestRegistry : FubuRegistry
+    {
+        public TestRegistry()
+        {
+            Applies.ToAssemblyContainingType<ConfigureFubuMvc>();
+
+            Actions.IncludeTypesNamed(n => n.EndsWith("Action"));
+
+            Routes
+                .HomeIs<ScoreBoardRequest>()
+                .IgnoreNamespaceForUrlFrom<UrlRoot>()
+                .IgnoreClassSuffix("Action")
+                .IgnoreMethodsNamed("Get")
+                .IgnoreMethodsNamed("Post")
+                .ConstrainToHttpMethod(c => c.Method.Name.Equals("Get"), "GET")
+                .ConstrainToHttpMethod(c => c.Method.Name.Equals("Post"), "POST");
+
+            Policies
+                .Add<AttachAuthenticationPolicy>()
+                .Add<AttachUserPolicy>()
+                .Add<AttachAdminPolicy>()
+                .WrapBehaviorChainsWith<RavenTransactionBehaviour>()
+                .WrapBehaviorChainsWith<load_the_current_principal>();
+
+            this.Validation(v =>
+            {
+                v.Actions
+                    .Include(a => a.HasInput && a.InputType().Name.EndsWith("ViewModel"));
+                v.Failures
+                    .If(a => a.InputType() != null && a.InputType().Name.EndsWith("ViewModel"))
+                    .TransferBy<ViewModelDescriptor>();
+            });
         }
     }
 }
